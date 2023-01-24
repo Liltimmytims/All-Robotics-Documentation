cbac08cf215ca1c6c726ea0584178f26a6024341
Liltimmytims <123426881+Liltimmytims@users.noreply.github.com>
2023-01-23 20:05:42 -0500

    Add files via upload
    First commit, got through part of joystick methods

---
+++ All Documentation (including unused methods).java
@@ -0,0 +1,139 @@
+//All Documentation (including unused methods)
+
+
+/////TO DO
+//import edu.wpi.first.wpilibj.TimedRobot;
+//import edu.wpi.first.wpilibj.CounterBase.EncodingType;
+//import edu.wpi.first.wpilibj.interfaces.Accelerometer;
+//import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+//import edu.wpi.first.wpilibj.AnalogInput;
+//import edu.wpi.first.wpilibj.Encoder;
+//import edu.wpi.first.wpilibj.Joystick;
+//import edu.wpi.first.wpilibj.PWM;
+//import edu.wpi.first.wpilibj.BuiltInAccelerometer;
+
+/////DONE
+//
+
+
+
+//////////[TITLE]//////////
+
+/////Import
+//
+
+/////Constructor
+
+/////Methods
+
+///Used:
+
+///Unused:
+
+
+
+//////////Timed Robot//////////
+
+/////Import
+//import edu.wpi.first.wpilibj.TimedRobot;
+
+/////Constructor
+
+/////Methods
+
+///Used:
+
+///Unused:
+
+
+
+
+//////////Joystick//////////
+
+/////Import
+//import edu.wpi.first.wpilibj.Joystick;
+
+/////Constructor
+//public Joystick​(int port)
+//Construct an instance of a joystick.
+//Parameters:
+//port - The port index on the Driver Station that the joystick is plugged into.
+
+/////Methods
+
+///Used:
+
+//public final double getX()
+//Get the X value of the joystick. This depends on the mapping of the joystick connected to the current port.
+//Returns: The X value of the joystick.
+
+//public final double getY()
+//Get the Y value of the joystick. This depends on the mapping of the joystick connected to the current port.
+//Returns: The Y value of the joystick.
+
+///Unused:
+
+//public void setXChannel​(int channel)
+//Set the channel associated with the X axis.
+//channel - The channel to set the axis to.
+
+//public void setYChannel​(int channel)
+//Set the channel associated with the Y axis.
+//channel - The channel to set the axis to.
+
+//public void setZChannel​(int channel)
+//Set the channel associated with the Z axis.
+//channel - The channel to set the axis to.
+
+//public void setThrottleChannel​(int channel)
+//Set the channel associated with the throttle axis.
+//channel - The channel to set the axis to.
+
+//public void setTwistChannel​(int channel)
+//Set the channel associated with the twist axis.
+//channel - The channel to set the axis to.
+
+//public int getXChannel()
+//Get the channel currently associated with the X axis.
+//Return: The channel for the axis.
+
+//public int getYChannel()
+//Get the channel currently associated with the Y axis.
+//Returns: The channel for the axis.
+
+//public int getZChannel()
+//Get the channel currently associated with the Z axis.
+//Returns: The channel for the axis.
+
+//public int getTwistChannel()
+//Get the channel currently associated with the twist axis.
+//Returns: The channel for the axis.
+
+//public int getThrottleChannel()
+//Get the channel currently associated with the throttle axis.
+//Returns: The channel for the axis.
+
+//public double getZ()
+//Get the z position of the HID.
+//Returns: the z position
+
+//public double getTwist()
+//Get the twist value of the current joystick. This depends on the mapping of the joystick connected to the current port.
+//Returns: The Twist value of the joystick.
+
+//public double getThrottle()
+//Get the throttle value of the current joystick. This depends on the mapping of the joystick connected to the current port.
+//Returns: The Throttle value of the joystick.
+
+//public boolean getTrigger()
+//Read the state of the trigger on the joystick.
+//Returns: The state of the trigger.
+
+//public boolean getTriggerPressed()
+//Whether the trigger was pressed since the last check.
+//Returns: Whether the button was pressed since the last check.
+
+//public boolean getTriggerReleased()
+//Whether the trigger was released since the last check.
+//Returns:
+//Whether the button was released since the last check.
