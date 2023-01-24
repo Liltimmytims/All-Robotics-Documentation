4f317d938b19f7d2ec63850be1cb08ddf43bdeaa
Liltimmytims <123426881+Liltimmytims@users.noreply.github.com>
2023-01-24 10:40:45 -0500

    Update All Documentation (including unused methods).java
    Finished WPI_TalonSRX

--- All Documentation (including unused methods).java
+++ All Documentation (including unused methods).java
@@ -2,7 +2,6 @@
 
 
 /////TO DO
-//import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 //import edu.wpi.first.wpilibj.AnalogInput;
 //import edu.wpi.first.wpilibj.Encoder;
 //import edu.wpi.first.wpilibj.PWM;
@@ -12,7 +11,7 @@
 //import edu.wpi.first.wpilibj.TimedRobot;
 //import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 //import edu.wpi.first.wpilibj.interfaces.Accelerometer;
-
+//import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 
 //import edu.wpi.first.wpilibj.Joystick;
@@ -117,11 +116,115 @@
 
 ///Used:
 
+//double getX()
+//Common interface for getting the x-axis acceleration.
+//Returns: The acceleration along the x-axis in g-forces
+
+//double getY()
+//Common interface for getting the y-axis acceleration.
+//Returns: The acceleration along the y-axis in g-forces
+
 ///Unused:
 
 //void setRange​(Accelerometer.Range range)
 //Common interface for setting the measuring range of an accelerometer.
 //range - The maximum acceleration, positive or negative, that the accelerometer will measure. Not all accelerometers support all ranges.
+
+//double getZ()
+//Common interface for getting the z axis acceleration.
+//Returns: The acceleration along the z axis in g-forces
+
+
+
+//////////WPI_TalonSRX//////////
+
+/////Import
+//import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+/////Constructor
+//public WPI_TalonSRX(int deviceNumber)
+//Constructor for motor controller
+//deviceNumber - device ID of motor controller
+
+/////Methods
+
+///Used:
+
+//public void set	(double speed)	
+//Common interface for setting the speed of a simple speed controller.
+//speed - The speed to set. Value should be between -1.0 and 1.0. Value is also saved for Get().
+
+//public void setInverted(boolean isInverted)	
+//Common interface for inverting direction of a speed controller.
+//isInverted - The state of inversion, true is inverted.
+
+
+///Unused:
+
+//public void close()
+
+//public void disable()	
+//Common interface for disabling a motor.
+
+//public void feed()	
+//Feed the motor safety object.
+//Resets the timer on this object that is used to do the timeouts.
+
+//public double get()	
+//Common interface for getting the current set speed of a speed controller.
+//Return: The current set speed. Value is between -1.0 and 1.0.
+
+//public String getDescription()	
+//Return: description of controller
+
+//public double getExpiration()	
+//Retrieve the timeout value for the corresponding motor safety object.
+//Return: the timeout value in seconds.
+
+//public boolean getInverted()	
+//Common interface for returning the inversion state of a speed controller.
+//Return: The state of inversion, true is inverted.
+
+//public void initSendable(SendableBuilder builder)	
+//Initialize sendable
+//builder - Base sendable to build on
+
+//public boolean isAlive()	
+//Determine of the motor is still operating or has timed out.
+//Return: a true value if the motor is still operating normally and hasn't timed out.
+
+//public boolean isSafetyEnabled()	
+//Return the state of the motor safety enabled flag.
+//Return if the motor safety is currently enabled for this device.
+//Return: True if motor safety is enforced for this device
+
+//public void set(ControlMode mode, double demand0, DemandType demand1Type, double demand1)
+//mode - Sets the appropriate output on the talon, depending on the mode.
+//demand0 - The output value to apply. such as advanced feed forward and/or auxiliary close-looping in firmware. In PercentOutput, the output is between -1.0 and 1.0, with 0.0 as stopped. In Current mode, output value is in amperes. In Velocity mode, output value is in position change / 100ms. In Position mode, output value is in encoder ticks or an analog value, depending on the sensor. See In Follower mode, the output value is the integer device ID of the talon to duplicate.
+//demand1Type - The demand type for demand1. Neutral: Ignore demand1 and apply no change to the demand0 output. AuxPID: Use demand1 to set the target for the auxiliary PID 1. ArbitraryFeedForward: Use demand1 as an arbitrary additive value to the demand0 output. In PercentOutput the demand0 output is the motor output, and in closed-loop modes the demand0 output is the output of PID0.
+//demand1 - Supplmental output value. Units match the set mode.
+
+//public void set(ControlMode mode, double value)		
+//Sets the appropriate output on the talon, depending on the mode.
+//mode - The output mode to apply. In PercentOutput, the output is between -1.0 and 1.0, with 0.0 as stopped. In Current mode, output value is in amperes. In Velocity mode, output value is in position change / 100ms. In Position mode, output value is in encoder ticks or an analog value, depending on the sensor. In Follower mode, the output value is the integer device ID of the talon to duplicate.
+//value - The setpoint value, as described above.
+
+//public void setExpiration(double expirationTime)	
+//Set the expiration time for the corresponding motor safety object.
+//expirationTime - The timeout value in seconds.
+
+//public void setSafetyEnabled(boolean enabled)	
+//Enable/disable motor safety for this device.
+//Turn on and off the motor safety option for this PWM object.
+//enabled - True if motor safety is enforced for this object
+
+//public void setVoltage(double outputVolts)	
+//Sets the voltage output of the SpeedController. Compensates for the current bus voltage to ensure that the desired voltage is output even if the battery voltage is below 12V - highly useful when the voltage outputs are "meaningful" (e.g. they come from a feedforward calculation).
+//NOTE: This function must be called regularly in order for voltage compensation to work properly - unlike the ordinary set function, it is not "set it and forget it."
+//outputVolts - The voltage to output.
+
+//public void stopMotor()	
+//Common interface to stop the motor until Set is called again.
 
 
 
